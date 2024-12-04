37f7e9b6ed8ff5467cb2496e187d7cc21a7a7ef3
root <feroza_ffs@hotmail.com>
2024-12-04 20:22:13 +0100

    setup release workflow
    updated ignore list
    
    fixed typo in release workflow
    
    added missing build dependencies
    
    fixed wayland dependency
    
    added missing vulkan
    
    added egl dependency
    
    corrected dependencies from gio website
    
    fixed artifact upload for release
    
    readjusted artifact location again
    
    fixed issue with zip files in build script
    
    merged release stages

---
+++ Build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+TARGETS=("linux/amd64" "windows/amd64")
+
+OUTPUT_DIR="./build"
+mkdir -p $OUTPUT_DIR
+
+for TARGET in "${TARGETS[@]}"; do
+    OS=$(echo $TARGET | cut -d'/' -f1)
+    ARCH=$(echo $TARGET | cut -d'/' -f2)
+    OSARCH="${OS}_${ARCH}"
+    EXTENSION=""
+    if [ "$OS" == "windows" ]; then
+        EXTENSION=".exe"
+    fi
+    echo "Building $OS/$ARCH..."
+
+    cd compare/src
+    echo "Building compare..."
+    GOOS=$OS GOARCH=$ARCH go build -o ../../$OUTPUT_DIR/$OSARCH/Compare$EXTENSION
+    cd ../..
+    cd filter/src
+    echo "Building filter..."
+    GOOS=$OS GOARCH=$ARCH go build -o ../../$OUTPUT_DIR/$OSARCH/Filter$EXTENSION
+    cd ../..
+    cd browser/src
+    echo "Building browser..."
+    GOOS=$OS GOARCH=$ARCH go build -o ../../$OUTPUT_DIR/$OSARCH/Browser$EXTENSION
+    cd ../..
+done
+
+cd $OUTPUT_DIR
+for folder in */; do
+    if [ "$(ls -A $folder)" ]; then
+        zip -r "${folder%/}.zip" "$folder"
+    fi
+done
+cd ..
